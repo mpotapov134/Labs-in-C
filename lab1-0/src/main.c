e56102dfe357a0de7af60505125c8106283b4ee1
m.potapov <m.potapov@g.nsu.ru>
2022-12-13 13:38:19 +0000

    Optimization, subtle code restructure

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -7,66 +7,15 @@
 typedef struct TextClass {
     unsigned char text[BUF_SIZE + 1];
     // checkPos stores the checking position in the current chunk of text and checkPosGlobal - in the text overall
-    size_t shift, checkPos, checkPosGlobal;
+    size_t shift, checkPos, checkPosGlobal, length;
 } TextClass;
 
 
 typedef struct SampleClass {
     unsigned char sample[17];
     int shiftTable[256];
+    size_t length;
 } SampleClass;
-
-
-int CanCheck(TextClass *textEntity) {
-    return textEntity->checkPos <= strlen((char*) textEntity->text);
-}
-
-
-void CheckMatch(TextClass *textEntity, SampleClass *sampleEntity) {
-    size_t sampleLen = strlen((char*) sampleEntity->sample);
-    for (unsigned i = 0; i < sampleLen; ++i) {
-        printf("%zu ", textEntity->checkPosGlobal - i);
-        if (textEntity->text[textEntity->checkPos - i - 1] != sampleEntity->sample[sampleLen - i - 1]) {
-            return;
-        }
-    }
-}
-
-
-void Refill(TextClass *textEntity, SampleClass *sampleEntity) {
-    textEntity->checkPos -= textEntity->shift;
-    size_t textLen = strlen((char*) textEntity->text);
-    size_t sampleLen = strlen((char*) sampleEntity->sample);
-    size_t tailLen = textLen - textEntity->checkPos; // defines the offset
-
-    strcpy((char*) textEntity->text, (char*) textEntity->text + textEntity->checkPos);
-    size_t bitesRead = fread(textEntity->text + tailLen, sizeof(char), BUF_SIZE - tailLen, stdin);
-    if (bitesRead < sampleLen) {
-        textEntity->text[0] = 0; // remaining text is shorter than the sample, no need to compare anymore
-    }
-    textEntity->text[bitesRead] = 0;
-    textEntity->checkPos = sampleLen;
-}
-
-
-void Proceed(TextClass *textEntity, SampleClass *sampleEntity) {
-    unsigned char firstCheckedSymb = textEntity->text[textEntity->checkPos - 1];
-    textEntity->shift = sampleEntity->shiftTable[(size_t) firstCheckedSymb];
-    textEntity->checkPos += textEntity->shift;
-    textEntity->checkPosGlobal += textEntity->shift;
-
-    if (!CanCheck(textEntity)) {
-        Refill(textEntity, sampleEntity);
-    }
-}
-
-
-void Search(TextClass *text, SampleClass *sample) {
-    while (CanCheck(text)) {
-        CheckMatch(text, sample);
-        Proceed(text, sample);
-    }
-}
 
 
 void MakeShiftTable(unsigned char *sample, int *resTable) {
@@ -77,13 +26,65 @@ void MakeShiftTable(unsigned char *sample, int *resTable) {
 }
 
 
+int CanCheck(TextClass *textEntity) {
+    return textEntity->checkPos <= textEntity->length; // means the sample can be placed in the text
+}
+
+
+void CheckMatch(TextClass *textEntity, SampleClass *sampleEntity) {
+    size_t sampleLen = sampleEntity->length;
+    for (unsigned i = 0; i < sampleLen; ++i) {
+        printf("%zu ", textEntity->checkPosGlobal - i);
+        if (textEntity->text[textEntity->checkPos - 1 - i] != sampleEntity->sample[sampleLen - 1 - i]) {
+            return;
+        }
+    }
+}
+
+
+void Refill(TextClass *textEntity, SampleClass *sampleEntity) {
+    textEntity->checkPos -= textEntity->shift;
+    size_t sampleLen = sampleEntity->length;
+    size_t offset = textEntity->length - textEntity->checkPos;
+
+    memmove(textEntity->text, textEntity->text + textEntity->checkPos, offset);
+    size_t bytesRead = fread(textEntity->text + offset, sizeof(char), BUF_SIZE - offset, stdin);
+    textEntity->text[bytesRead + offset] = 0;
+    textEntity->length = bytesRead + offset;
+    textEntity->checkPos = sampleLen;
+}
+
+
+void Proceed(TextClass *textEntity, SampleClass *sampleEntity) {
+    unsigned char firstCheckedSymb = textEntity->text[textEntity->checkPos - 1];
+    textEntity->shift = sampleEntity->shiftTable[(size_t) firstCheckedSymb];
+    textEntity->checkPos += textEntity->shift;
+    textEntity->checkPosGlobal += textEntity->shift;
+}
+
+
+void Search(TextClass *textEntity, SampleClass *sampleEntity) {
+    while (CanCheck(textEntity)) {
+        CheckMatch(textEntity, sampleEntity);
+        Proceed(textEntity, sampleEntity);
+        if (!CanCheck(textEntity)) {
+            Refill(textEntity, sampleEntity);
+        }
+    }
+}
+
+
 int main(void) {
     unsigned char sample[18];
-    if (!fgets((char*) sample, 18, stdin)) {
+    if (!fgets((char*) sample, sizeof(sample), stdin)) {
         exit(0);
     }
     *strchr((char*) sample, '\n') = 0;
     size_t sampleLen = strlen((char*) sample);
+
+    unsigned char text[BUF_SIZE + 1];
+    size_t bytesRead = fread(text, sizeof(char), BUF_SIZE, stdin);
+    text[bytesRead] = 0;
 
     SampleClass sampleEntity;
     strcpy((char*) sampleEntity.sample, (char*) sample);
@@ -91,15 +92,9 @@ int main(void) {
         sampleEntity.shiftTable[i] = sampleLen;
     }
     MakeShiftTable(sample, sampleEntity.shiftTable);
+    sampleEntity.length = sampleLen;
 
-    unsigned char text[BUF_SIZE + 1];
-    size_t bitesRead = fread(text, sizeof(char), BUF_SIZE, stdin);
-    if (bitesRead < sampleLen) {
-        exit(0);
-    }
-    text[bitesRead] = 0;
-
-    TextClass textEntity = {"", 0, sampleLen, sampleLen};
+    TextClass textEntity = {"", 0, sampleLen, sampleLen, bytesRead};
     strcpy((char*) textEntity.text, (char*) text);
 
     Search(&textEntity, &sampleEntity);
