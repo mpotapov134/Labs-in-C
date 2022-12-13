1b2f5f38ed8e000cc8e7558cb52aff08b956bb06
m.potapov <m.potapov@g.nsu.ru>
2022-12-13 12:42:22 +0000

    Reorganized the code and added comments

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -29,11 +29,6 @@ int CalcHash(char *string, size_t chunkLength) {
 }
 
 
-int CanCheck(TextClass *textEntity, SampleClass *sampleEntity) {
-    return textEntity->length >= sampleEntity->length;
-}
-
-
 void CheckMatch(TextClass *textEntity, SampleClass *sampleEntity) {
     if (textEntity->hash != sampleEntity->hash) {
         return;
@@ -51,37 +46,38 @@ void Refill(TextClass *textEntity, SampleClass *sampleEntity) {
     size_t chunkLength = sampleEntity->length;
     size_t offset = chunkLength - 1;
     memmove(textEntity->text, textEntity->text + textEntity->length - offset, offset);
-    size_t bitesRead = fread(textEntity->text + offset, sizeof(char), BUF_SIZE - offset, stdin);
-    textEntity->text[bitesRead + offset] = 0;
-    textEntity->length = bitesRead + offset;
+    size_t bytesRead = fread(textEntity->text + offset, sizeof(char), BUF_SIZE - offset, stdin);
+    textEntity->text[bytesRead + offset] = 0;
+    textEntity->length = bytesRead + offset;
 }
 
 
-int Proceed(TextClass *textEntity, SampleClass *sampleEntity) {
+int MoveIfPossible(TextClass *textEntity, SampleClass *sampleEntity) {
     size_t chunkLength = sampleEntity->length;
-    textEntity->hash /= 3;
     textEntity->index ++;
     textEntity->indexGlobal ++;
+    int indOfLast = textEntity->index - 1 + chunkLength - 1; // index of the last symbol in the text to be compared with the sample
 
-    int indOfLast = textEntity->index - 1 + chunkLength - 1;
-    if (indOfLast >= (int) textEntity->length) {
+    if (indOfLast >= textEntity->length) { // the buffer is too short to check, need to refill
         Refill(textEntity, sampleEntity);
-        if (!CanCheck(textEntity, sampleEntity)) {
-            return 0;
-        }
         textEntity->index = 1;
         indOfLast = chunkLength - 1;
+        if (indOfLast >= textEntity->length) {
+            return 0; // refilling the buffer didn't help, can't proceed anymore
+        }
     }
+
+    textEntity->hash /= 3;
     textEntity->hash += ((unsigned char) textEntity->text[indOfLast] % 3) * pow(3, chunkLength - 1);
-    return 1;
+    return 1; // the buffer is still long enough, so we can proceed successfully
 }
 
 
 void Search(TextClass *textEntity, SampleClass *sampleEntity) {
-    int flag = CanCheck(textEntity, sampleEntity);
-    while (flag) {
+    int canMove = textEntity->length >= sampleEntity->length;
+    while (canMove) {
         CheckMatch(textEntity, sampleEntity);
-        flag = Proceed(textEntity, sampleEntity);
+        canMove = MoveIfPossible(textEntity, sampleEntity);
     }
 }
 
@@ -90,23 +86,21 @@ int main(void) {
     setlocale(LC_ALL, "Russian");
 
     char sample[18];
-    if (!fgets(sample, 18, stdin)) {
+    if (!fgets(sample, sizeof(sample), stdin)) {
         exit(0);
     }
     *strchr(sample, '\n') = 0;
-    int sampleHash = CalcHash(sample, strlen(sample));
+    size_t sampleLen = strlen(sample);
+    int sampleHash = CalcHash(sample, sampleLen);
     printf("%i ", sampleHash);
 
     char text[BUF_SIZE + 1];
-    size_t bitesRead = fread(text, sizeof(char), BUF_SIZE, stdin);
-    if (bitesRead < strlen(sample)) {
-        exit(0);
-    }
-    text[bitesRead] = 0;
+    size_t bytesRead = fread(text, sizeof(char), BUF_SIZE, stdin);
+    text[bytesRead] = 0;
 
-    TextClass textEntity = {"", CalcHash(text, strlen(sample)), 1, 1, bitesRead};
+    TextClass textEntity = {"", CalcHash(text, sampleLen), 1, 1, bytesRead};
     strcpy(textEntity.text, text);
-    SampleClass sampleEntity = {"", sampleHash, strlen(sample)};
+    SampleClass sampleEntity = {"", sampleHash, sampleLen};
     strcpy(sampleEntity.sample, sample);
 
     Search(&textEntity, &sampleEntity);
