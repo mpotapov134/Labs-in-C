7ec232ecaa5bf3b653b24d32b01d1cccb37af0b6
m.potapov <m.potapov@g.nsu.ru>
2023-02-22 04:39:10 +0000

    Wrote lab6-0

--- lab6-0/src/main.c
+++ lab6-0/src/main.c
@@ -1,5 +1,90 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef struct AVLTree {
+    int value;
+    unsigned char height;
+    struct AVLTree* left;
+    struct AVLTree* right;
+} Tree;
+
+unsigned char Height(Tree* tree) {
+    return tree ? tree->height:0;
+}
+
+void UpdateHeight(Tree* tree) {
+    unsigned char left = Height(tree->left), right = Height(tree->right);
+    tree->height = ((left > right) ? left:right) + 1;
+}
+
+int BalanceFactor(Tree* tree) {
+    return Height(tree->right) - Height(tree->left);
+}
+
+Tree* RotateRight(Tree* root) {
+    Tree* left = root->left;
+    root->left = left->right;
+    left->right = root;
+    UpdateHeight(root);
+    UpdateHeight(left);
+    return left;
+}
+
+Tree* RotateLeft(Tree* root) {
+    Tree* right = root->right;
+    root->right = right->left;
+    right->left = root;
+    UpdateHeight(root);
+    UpdateHeight(right);
+    return right;
+}
+
+Tree* Balance(Tree* root) {
+    UpdateHeight(root);
+    if (BalanceFactor(root) == 2) {
+        if (BalanceFactor(root->right) < 0) {
+            root->right = RotateRight(root->right);
+        }
+        return RotateLeft(root);
+    }
+    if (BalanceFactor(root) == -2) {
+        if (BalanceFactor(root->left) > 0) {
+            root->left = RotateLeft(root->left);
+        }
+        return RotateRight(root);
+    }
+    return root;
+}
+
+Tree* Insert(Tree* originalStart, Tree* tree, int value, int treeArrayIndex) {
+    if (!tree) {
+        Tree* newNode = &originalStart[treeArrayIndex];
+        newNode->value = value;
+        newNode->height = 1;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+    if (value < tree->value) {
+        tree->left = Insert(originalStart, tree->left, value, treeArrayIndex);
+    }
+    else {
+        tree->right = Insert(originalStart, tree->right, value, treeArrayIndex);
+    }
+    tree = Balance(tree);
+    return tree;
+}
 
 int main(void) {
-    return EXIT_SUCCESS;
+    int n, value;
+    if (!scanf("%i", &n)) exit(0);
+    Tree* originalStart = (Tree*) malloc(sizeof(Tree) * n);
+    Tree* tree = NULL;
+
+    for (int i = 0; i < n; ++i) {
+        if (!scanf("%i", &value)) exit(0);
+        tree = Insert(originalStart, tree, value, i);
+    }
+    printf("%u\n", Height(tree));
+    free(originalStart);
 }
