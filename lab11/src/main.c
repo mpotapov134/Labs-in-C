c35d0ee91b0922da10b3cb13fa20da3a2e50ad82
m.potapov <m.potapov@g.nsu.ru>
2023-03-25 07:45:08 +0000

    Typos fixed

--- lab11/src/main.c
+++ lab11/src/main.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #define MAX(a, b) (a >= b) ? a : b
 
-void PrintItemsInfo(int totalSize, int count, int size, int weighs[], int prices[], int costMatrix[][totalSize + 1]) { // totalSize is only used to define the matrix size
+void PrintItemsInfo(int totalSize, int count, int size, int weights[], int prices[], int costMatrix[][totalSize + 1]) { // totalSize is only used to define the matrix size
     if (!costMatrix[count][size]) return;
 
     if (costMatrix[count][size] == costMatrix[count - 1][size]) { // сумма осталась та же, значит, элемент с номером count не положили
-        PrintItemsInfo(totalSize, count - 1, size, weighs, prices, costMatrix);
+        PrintItemsInfo(totalSize, count - 1, size, weights, prices, costMatrix);
     }
     else {
-        PrintItemsInfo(totalSize, count - 1, size - weighs[count - 1], weighs, prices, costMatrix);
-        printf("%i %i\n", weighs[count - 1], prices[count - 1]);
+        PrintItemsInfo(totalSize, count - 1, size - weights[count - 1], weights, prices, costMatrix);
+        printf("%i %i\n", weights[count - 1], prices[count - 1]);
     }
 }
 
-void CalcMaxCost(int totalNumber, int totalSize, int weighs[], int prices[], int costMatrix[][totalSize + 1]) {
+void CalcMaxCost(int totalNumber, int totalSize, int weights[], int prices[], int costMatrix[][totalSize + 1]) {
     for (int i = 0; i <= totalNumber; ++i) costMatrix[i][0] = 0; // если вместимость == 0, итоговая стоимость будет == 0
     for (int i = 0; i <= totalSize; ++i) costMatrix[0][i] = 0; // если можно положить 0 первых предметов, итоговая стоимость будет == 0
 
     for (int count = 1; count <= totalNumber; ++count) {
         for (int size = 1; size <= totalSize; ++size) {
-            if (size >= weighs[count - 1]) {
-                costMatrix[count][size] = MAX(costMatrix[count - 1][size], costMatrix[count - 1][size - weighs[count - 1]] + prices[count - 1]);
+            if (size >= weights[count - 1]) {
+                costMatrix[count][size] = MAX(costMatrix[count - 1][size], costMatrix[count - 1][size - weights[count - 1]] + prices[count - 1]);
             }
             else costMatrix[count][size] = costMatrix[count - 1][size];
         }
@@ -32,15 +32,15 @@ int main(void) {
     int totalNumber, totalSize;
     if (2 != scanf("%i %i", &totalNumber, &totalSize)) exit(0);
 
-    int weighs[totalNumber], prices[totalNumber];
+    int weights[totalNumber], prices[totalNumber];
     for (int i = 0; i < totalNumber; ++i) {
-        if (2 != scanf("%i %i", &weighs[i], &prices[i])) exit(0);
+        if (2 != scanf("%i %i", &weights[i], &prices[i])) exit(0);
     }
 
     int costMatrix[totalNumber + 1][totalSize + 1];
-    CalcMaxCost(totalNumber, totalSize, weighs, prices, costMatrix);
+    CalcMaxCost(totalNumber, totalSize, weights, prices, costMatrix);
 
     printf("%i\n", costMatrix[totalNumber][totalSize]);
 
-    PrintItemsInfo(totalSize, totalNumber, totalSize, weighs, prices, costMatrix);
+    PrintItemsInfo(totalSize, totalNumber, totalSize, weights, prices, costMatrix);
 }
